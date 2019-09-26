71c44a743ef8f256e3f4b1a79f4aaef3bff1f147
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-09-26 01:41:19 -0700

    date and time changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,10 +10,11 @@ class Comment < ApplicationRecord
 
   scope :today, -> { where(created_at: (Time.now.beginning_of_day..Time.now.end_of_day)) }
 
+  # scope :user_email, -> { where(user_id: user.id)} }
+
   private
 
   def comment_limit
-    post_id
     if user.comments.today.count >= 2
       errors.add(:base, 'You can only comment on two blogs per day')
     end
